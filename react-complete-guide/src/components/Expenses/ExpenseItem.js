34fe2cdce00c3070be385df3574c517c121b8026
Erika Nielsen <enielsen0001@gmail.com>
2022-02-25 15:08:11 -0500

    removed title update saved state causing bugs

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ExpenseItem.scss";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated");
-    console.log(title); // this will still have the old value as setState schedules the update
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
