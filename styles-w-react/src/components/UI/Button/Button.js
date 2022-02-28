5acb5c15552ea67b9d1604cca0f6c612eff7fb6e
Erika Nielsen <enielsen0001@gmail.com>
2022-02-28 13:22:33 -0500

    using css modules for scoped styles

--- styles-w-react/src/components/UI/Button/Button.js
+++ styles-w-react/src/components/UI/Button/Button.js
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+// import styled from "styled-components";
+import React from "react";
+
+import styles from "./Button.module.scss";
 
 // styled.button`` is a tagged template literal
 // button is amethod on this styled Component
@@ -6,30 +9,37 @@ import styled from "styled-components";
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 
 // [this will return a button with the styles defined in between the back ticks]
-const Button = styled.button`
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
-  width: 100%;
+// const Button = styled.button`
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+//   width: 100%;
 
-  @media (min-width: 768px) {
-    width: auto;
-  }
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
 
-  &:focus {
-    outline: none;
-  }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 export default Button;
