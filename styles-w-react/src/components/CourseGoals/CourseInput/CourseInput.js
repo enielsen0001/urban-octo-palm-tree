23950fa7e1611be1c1badf9f6a3ab7ea9bea36ab
Erika Nielsen <enielsen0001@gmail.com>
2022-02-28 12:23:18 -0500

    add conditional inline styles

--- styles-w-react/src/components/CourseGoals/CourseInput/CourseInput.js
+++ styles-w-react/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    // checkl if something was entered
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+
+      // prevent onAddGoal from running below
+      return;
+    }
+
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            "border-color": !isValid ? "red" : "black",
+            "background-color": !isValid ? "pink" : "white",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
