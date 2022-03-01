1c427d95ba818f8d1725ef1c3162ec9fe5518bb3
Erika Nielsen <enielsen0001@gmail.com>
2022-03-01 17:12:02 -0500

    refs

--- fragments-portals-refs/src/components/Users/AddUser.js
+++ fragments-portals-refs/src/components/Users/AddUser.js
@@ -1,43 +1,53 @@
 import React, { useState } from "react";
+import { useRef } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css";
-import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  // https://reactjs.org/docs/refs-and-the-dom.html
+
+  // if you only want to read a value and not change anything, then refs
+  // are probably better than using states
+
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+
+    // these is the actual DOM nodes
+    console.log(nameInputRef, ageInputRef);
+
+    // this pevents need for state, only read on submit, not on every change
+    console.log(nameInputRef.current.value, ageInputRef.current.value);
+
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: "Invalid input",
         message: "Please enter a valid name and age (non-empty values).",
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: "Invalid age",
         message: "Please enter a valid age (> 0).",
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername("");
-    setEnteredAge("");
-  };
+    props.onAddUser(enteredName, enteredUserAge);
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
+    // only use this heavy handed thing with inputs only
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   const errorHandler = () => {
@@ -45,98 +55,6 @@ const AddUser = (props) => {
   };
 
   return (
-    // you can either use a wrapping element to return just one item to react
-    // this can lead to lots of extra divs all over the place
-    // <div>
-    //   {error && (
-    //     <ErrorModal
-    //       title={error.title}
-    //       message={error.message}
-    //       onConfirm={errorHandler}
-    //     />
-    //   )}
-    //   <Card className={classes.input}>
-    //     <form onSubmit={addUserHandler}>
-    //       <label htmlFor="username">Username</label>
-    //       <input
-    //         id="username"
-    //         type="text"
-    //         value={enteredUsername}
-    //         onChange={usernameChangeHandler}
-    //       />
-    //       <label htmlFor="age">Age (Years)</label>
-    //       <input
-    //         id="age"
-    //         type="number"
-    //         value={enteredAge}
-    //         onChange={ageChangeHandler}
-    //       />
-    //       <Button type="submit">Add User</Button>
-    //     </form>
-    //   </Card>
-    // </div>
-
-    // or return an array to wrap more than one element, but in that case you need ot add keys to each elemtn in the array
-    // [
-    //   error && (
-    //     <ErrorModal
-    //       key="error-modal"
-    //       title={error.title}
-    //       message={error.message}
-    //       onConfirm={errorHandler}
-    //     />
-    //   ),
-    //   <Card key="imput-card" className={classes.input}>
-    //     <form onSubmit={addUserHandler}>
-    //       <label htmlFor="username">Username</label>
-    //       <input
-    //         id="username"
-    //         type="text"
-    //         value={enteredUsername}
-    //         onChange={usernameChangeHandler}
-    //       />
-    //       <label htmlFor="age">Age (Years)</label>
-    //       <input
-    //         id="age"
-    //         type="number"
-    //         value={enteredAge}
-    //         onChange={ageChangeHandler}
-    //       />
-    //       <Button type="submit">Add User</Button>
-    //     </form>
-    //   </Card>,
-    // ]
-
-    // use a wrapper helper component to prevent unnecessary html markup
-    // <Wrapper>
-    //   {error && (
-    //     <ErrorModal
-    //       title={error.title}
-    //       message={error.message}
-    //       onConfirm={errorHandler}
-    //     />
-    //   )}
-    //   <Card className={classes.input}>
-    //     <form onSubmit={addUserHandler}>
-    //       <label htmlFor="username">Username</label>
-    //       <input
-    //         id="username"
-    //         type="text"
-    //         value={enteredUsername}
-    //         onChange={usernameChangeHandler}
-    //       />
-    //       <label htmlFor="age">Age (Years)</label>
-    //       <input
-    //         id="age"
-    //         type="number"
-    //         value={enteredAge}
-    //         onChange={ageChangeHandler}
-    //       />
-    //       <Button type="submit">Add User</Button>
-    //     </form>
-    //   </Card>
-    // </Wrapper>
-
     <React.Fragment>
       {error && (
         <ErrorModal
@@ -148,19 +66,9 @@ const AddUser = (props) => {
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
-          />
+          <input id="username" type="text" ref={nameInputRef} />
           <label htmlFor="age">Age (Years)</label>
-          <input
-            id="age"
-            type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
-          />
+          <input id="age" type="number" ref={ageInputRef} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
