38b4b040f91f23ca7cd28e359d55ada74d64b55e
Erika Nielsen <enielsen0001@gmail.com>
2022-03-03 10:09:12 -0500

    useEffect

---
+++ useeffect-reducers-contextapi/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // this creates an infinite loop bc it uses setState which causes a reeval
+  // if (storedLoginInfo === "1") {
+  //   setIsLoggedIn(true);
+  // }
+
+  // this only runs once on page load because it doesn't have any dependancies assigned
+  useEffect(() => {
+    const storedLoginInfo = localStorage.getItem("isLoggedIn");
+
+    if (storedLoginInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.setItem("isLoggedIn", "0");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
