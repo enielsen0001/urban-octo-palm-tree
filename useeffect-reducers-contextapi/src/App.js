a64cb21614af19f792d533abd522809fe2ce6516
Erika Nielsen <enielsen0001@gmail.com>
2022-03-03 14:20:14 -0500

    sdcs;jn

--- useeffect-reducers-contextapi/src/App.js
+++ useeffect-reducers-contextapi/src/App.js
@@ -1,45 +1,19 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useContext } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  // this creates an infinite loop bc it uses setState which causes a reeval
-  // if (storedLoginInfo === "1") {
-  //   setIsLoggedIn(true);
-  // }
-
-  // this only runs once on page load because it doesn't have any dependancies assigned
-  useEffect(() => {
-    const storedLoginInfo = localStorage.getItem("isLoggedIn");
-
-    if (storedLoginInfo === "1") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.setItem("isLoggedIn", "0");
-    setIsLoggedIn(false);
-  };
+  const ctx = useContext(AuthContext);
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
     </React.Fragment>
   );
